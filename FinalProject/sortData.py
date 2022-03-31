2a0fe1c5694ace849088a0ed11a21a0815fbbd1f
Alexantoun <oz.antoun@gmail.com>
2022-03-30 20:58:47 -0600

    foudn more data that could be used in sensReadings

--- FinalProject/sortData.py
+++ FinalProject/sortData.py
@@ -1,31 +1,37 @@
 import pandas as pd
-
 df = pd.read_excel('fileName.xlsx')
 
-class Node:
+class sensReading:
     def __init__(self):
-        #find all the values that need to be placed inside a node
+        #-9999 used a sentinel value to flag error
         self.temp = -9999
+        self.FDOY = -9999
+        self.PAppbv = -9999
+        self._49Cppbv = -9999
         self.next = None
 
-class sortedLinkedList:
-    def __init__(self):
-        self.head = None    
-    #May not be able to shove in an entire node
-    def addNode(self, newNode):
-        current = self.head    
-        if current is not None:
-            while current.next is not None:
-                if current.next.temp > newNode.temp:
-                    break
-                current = current.next
+#class sortedLinkedList:
+#    def __init__(self):
+#        self.head = None    
+    #May not be able to shove in an entire node into a list so keep this
+#    def addNode(self, newNode):
+#        current = self.head    
+#        if current is not None:
+#            if current.temp > newNode.temp:
+#               newNode.next = current
+#                self.head = newNode
+#            else:
+#                while current.next is not None:
+#                    if current.next.temp > newNode.temp:
+#                        break
+#                   current = current.next
 
-            newNode.next = current.next
-            current.next = newNode
-            return 
+#                newNode.next = current.next
+#                current.next = newNode
+#            return 
 
-        elif current is None:
-            self.head = newNode
-        return
+#        elif current is None:
+#            self.head = newNode
+#        return
 
 
