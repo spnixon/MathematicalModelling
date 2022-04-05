ab119a0eca59300252adfeb7b5f43853add900b1
Alexantoun <oz.antoun@gmail.com>
2022-04-05 14:27:42 -0600

    Made it much fast

--- FinalProject/sortData.py
+++ FinalProject/sortData.py
@@ -14,19 +14,26 @@ class sensReading:
 
 def addItem(sensorData:deque, newNode:sensReading):
     if len(sensorData) != 0:
+
         if sensorData[-1].PAppbv < newNode.PAppbv:
             sensorData.append(newNode)
+
         elif newNode.PAppbv < sensorData[0].PAppbv:
             sensorData.appendleft(newNode)
+
         else:
-            i = 0
-            while sensorData[i].PAppbv < newNode.PAppbv:
-                i += 1
-            sensorData.insert(i, newNode)
+            x = 0
+            while ( x+100 < len(sensorData)-1 ) and ( sensorData[x+100].PAppbv < newNode.PAppbv ):
+                x+=100
+            while( x+10 < len(sensorData)-1 ) and ( sensorData[x+10].PAppbv < newNode.PAppbv ):
+                x+=10
+            while sensorData[x].PAppbv < newNode.PAppbv:
+                x += 1
+            sensorData.insert(x, newNode)
+
     elif len(sensorData) == 0:
         sensorData.append(newNode)
     
-
 def sortBins(sensorData:deque, start:int, end:int):
     for i in sensorData:
         if i.temp <= end and i.temp >= start:
@@ -38,15 +45,18 @@ df = pd.read_csv('TempBins_01.csv')
 sensorData = deque()
 readData = list()
 sensorData.clear()
-for i in range (0, 1500):
-    print(i)
+progress = 0
+for i in range (0, 42545):
     readData = df.iloc[i]
     newNode = sensReading()
     newNode.FDOY = readData[0]
     newNode._49Cppbv = readData[1]
+    if i%4300 == 0:
+        progress+=10
+        print(progress,'%')
     newNode.PAppbv = readData[2]
     newNode.temp = readData[3]
     addItem(sensorData, newNode)
 print("Done")
-for x in range (0, 1500):
+for x in range (0, 42545):
     sensorData[x].print()
